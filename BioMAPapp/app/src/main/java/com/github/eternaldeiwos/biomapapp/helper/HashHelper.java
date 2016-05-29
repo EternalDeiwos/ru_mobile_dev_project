63bdb6bc79bb99d312b3c7b6fe0da9043a477e82
Greg Linklater <greglink49@gmail.com>
2016-05-29 12:05:08 +0200

    data verification utils

--- BioMAPapp/app/src/main/java/com/github/eternaldeiwos/biomapapp/helper/HashHelper.java
+++ BioMAPapp/app/src/main/java/com/github/eternaldeiwos/biomapapp/helper/HashHelper.java
@@ -10,6 +10,7 @@ import java.util.Map;
  */
 public final class HashHelper {
     private HashHelper() {} // static class
+    private static HashMap<String, String> hashes; // replace with thread-safe persistence later
 
     public static String hashMD5(String plain_text) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("MD5");
@@ -19,5 +20,24 @@ public final class HashHelper {
     public static String hashSHA1(String plain_text) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("SHA-1");
         return new String(md.digest(plain_text.getBytes()));
+    }
+
+    private static Map<String, String> getHashes() {
+        if (hashes == null) hashes = new HashMap<>();
+        return hashes;
+    }
+
+    public static boolean isDataChanged(String key, String data) throws NoSuchAlgorithmException {
+        if (getHashes().containsKey(key))
+            if (getHashes().get(key).equals(hashSHA1(data))) return false;
+        return true;
+    }
+
+    public static void updateHash (String key, String data) throws NoSuchAlgorithmException {
+        getHashes().put(key, hashSHA1(data));
+    }
+
+    public static String getHash (String key) {
+        return getHashes().get(key);
     }
 }
