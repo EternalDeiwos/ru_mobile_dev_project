8010368551e1953d40477a33dcb9c31cded9a8e0
Greg Linklater <greglink49@gmail.com>
2016-05-30 11:59:53 +0200

    Insert record API

--- BioMAPapp/app/src/main/java/com/github/eternaldeiwos/biomapapp/model/Record.java
+++ BioMAPapp/app/src/main/java/com/github/eternaldeiwos/biomapapp/model/Record.java
@@ -1,5 +1,7 @@
 package com.github.eternaldeiwos.biomapapp.model;
 
+import android.net.Uri;
+
 import com.orm.SugarRecord;
 
 import java.net.URI;
@@ -53,8 +55,8 @@ public class Record extends SugarRecord {
         }
     }
 
-    public URI[] images;
-    public URI sound;
+    public Uri[] images;
+    public Uri sound;
     public String username;
     public String userid;
     public String email;
@@ -64,24 +66,92 @@ public class Record extends SugarRecord {
     public String province;
     public String nearesttown;
     public String locality;
-    public int min_elev;
-    public int max_elev;
+    public int minelev;
+    public int maxelev;
     public float lat;
     public float lng;
     public int accuracy;
-    public String gps_source;
+    public String source;
     public int year;
     public int month;
     public int day;
-    public String observer_note;
-    public String user_identification;
-    public int nest_count;
-    public String nest_site;
+    public String note;
+    public String userdet;
+    public int nestcount;
+    public String nestsite;
     public boolean roadkill;
     public String taxonid;
-    public String scientific_name;
-    public String institution;
+    public String taxonname;
+    public String institution_code;
     public String collection_code;
-    public String record_basis;
-    public OccurrenceStatus record_status;
+    public String recordbasis;
+    public OccurrenceStatus recordstatus;
+
+    public Record(
+            Uri[] images,
+            Uri sound,
+            String username,
+            String userid,
+            String email,
+            String project,
+            String observers,
+            String country,
+            String province,
+            String nearesttown,
+            String locality,
+            int minelev,
+            int maxelev,
+            float lat,
+            float lng,
+            int accuracy,
+            String source,
+            int year,
+            int month,
+            int day,
+            String note,
+            String userdet,
+            int nestcount,
+            String nestsite,
+            boolean roadkill,
+            String taxonid,
+            String taxonname,
+            String institution_code,
+            String collection_code,
+            String recordbasis,
+            OccurrenceStatus recordstatus
+    ) {
+        this.images = images;
+        this.sound = sound;
+        this.username = username;
+        this.userid = userid;
+        this.email = email;
+        this.project = project;
+        this.observers = observers;
+        this.country = country;
+        this.province = province;
+        this.nearesttown = nearesttown;
+        this.locality = locality;
+        this.minelev = minelev;
+        this.maxelev = maxelev;
+        this.lat = lat;
+        this.lng = lng;
+        this.accuracy = accuracy;
+        this.source = source;
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.note = note;
+        this.userdet = userdet;
+        this.nestcount = nestcount;
+        this.nestsite = nestsite;
+        this.roadkill = roadkill;
+        this.taxonid = taxonid;
+        this.taxonname = taxonname;
+        this.institution_code = institution_code;
+        this.collection_code = collection_code;
+        this.recordbasis = recordbasis;
+        this.recordstatus = recordstatus;
+    }
+
+    public Record() { /* default */ }
 }
--- BioMAPapp/app/src/main/java/com/github/eternaldeiwos/biomapapp/rest/RestUpload.java
+++ BioMAPapp/app/src/main/java/com/github/eternaldeiwos/biomapapp/rest/RestUpload.java
@@ -1,10 +1,20 @@
 package com.github.eternaldeiwos.biomapapp.rest;
 
+import android.content.Context;
+import android.net.Uri;
+
 import com.github.eternaldeiwos.biomapapp.helper.API;
 import com.github.eternaldeiwos.biomapapp.model.Record;
+import com.ipaulpro.afilechooser.utils.FileUtils;
 
+import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.LinkedList;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
@@ -21,42 +31,45 @@ import retrofit2.http.Part;
 
 public class RestUpload {
     private static IRestUpload uploadService;
+    private static MediaType formdatatype;
 
     public interface IRestUpload {
         @Multipart
         @POST("insertrecord")
         Call<ResponseBody> upload(
-                @Part("images[]") RequestBody image,
-                @Part("sound") RequestBody sound,
-                @Part("username") String username,
-                @Part("userid") String adu_number,
-                @Part("email") String email,
-                @Part("project") String project_name,
-                @Part("observers") String observers,
-                @Part("country") String country,
-                @Part("province") String province,
-                @Part("nearesttown") String town,
-                @Part("locality") String locality,
-                @Part("minelev") int min_elevation,
-                @Part("maxelev") int max_elevation,
-                @Part("lat") float lat,
-                @Part("long") float lng,
-                @Part("accuracy") int accuracy,
-                @Part("source") String gps_source,
-                @Part("year") int year,
-                @Part("month") int month,
-                @Part("day") int day,
-                @Part("note") String observer_note,
-                @Part("userdet") String user_identification,
-                @Part("nest_count") int nest_count,
-                @Part("nest_site") String nest_site,
-                @Part("roadkill") int roadkill_bool,
-                @Part("taxonid") String taxonid,
-                @Part("taxonname") String scientific_name,
-                @Part("institution_code") String institution,
-                @Part("collection") String collection_code,
-                @Part("recordbasis") String basis_of_record, // SIGHTING or VMUS:CAMERA TRAP or VMUS
-                @Part("recordstatus") String status // 1) blank, 2) naturally occuring,
+                @Part("images[]") MultipartBody.Part image1,
+                @Part("images[]") MultipartBody.Part image2,
+                @Part("images[]") MultipartBody.Part image3,
+                @Part("sound") MultipartBody.Part sound,
+                @Part("username") RequestBody username,
+                @Part("userid") RequestBody userid,
+                @Part("email") RequestBody email,
+                @Part("project") RequestBody project,
+                @Part("observers") RequestBody observers,
+                @Part("country") RequestBody country,
+                @Part("province") RequestBody province,
+                @Part("nearesttown") RequestBody nearesttown,
+                @Part("locality") RequestBody locality,
+                @Part("minelev") RequestBody minelev,
+                @Part("maxelev") RequestBody maxelev,
+                @Part("lat") RequestBody lat,
+                @Part("long") RequestBody lng,
+                @Part("accuracy") RequestBody accuracy,
+                @Part("source") RequestBody source,
+                @Part("year") RequestBody year,
+                @Part("month") RequestBody month,
+                @Part("day") RequestBody day,
+                @Part("note") RequestBody note, // observer note
+                @Part("userdet") RequestBody userdet, // user identification
+                @Part("nest_count") RequestBody nestcount,
+                @Part("nest_site") RequestBody nestsite,
+                @Part("roadkill") RequestBody roadkill_bool,
+                @Part("taxonid") RequestBody taxonid,
+                @Part("taxonname") RequestBody taxonname,
+                @Part("institution_code") RequestBody institution_code,
+                @Part("collection") RequestBody collection_code,
+                @Part("recordbasis") RequestBody recordbasis, // SIGHTING or VMUS:CAMERA TRAP or VMUS
+                @Part("recordstatus") RequestBody recordstatus // 1) blank, 2) naturally occuring,
                 // 3) re-introduced, 4) introduced, 5) feral, 6) cultivated, 7) exotic
         );
     }
@@ -72,14 +85,14 @@ public class RestUpload {
         return uploadService;
     }
 
-    public static void uploadRecord(Record record, Callback<ResponseBody> callback) {
-        Call<ResponseBody> call = buildRequest(record, getInstance());
+    public static void uploadRecord(Context context, Record record, Callback<ResponseBody> callback) {
+        Call<ResponseBody> call = buildRequest(context, record, getInstance());
         call.enqueue(callback);
     }
 
-    public static boolean uploadRecord(Record record) {
+    public static boolean uploadRecord(Context context, Record record) {
         try {
-            Response response = buildRequest(record, getInstance()).execute().raw();
+            Response response = buildRequest(context, record, getInstance()).execute().raw();
             System.err.println(response.body().string());
             return response.code() == 200;
         } catch (IOException ioe) {
@@ -88,11 +101,143 @@ public class RestUpload {
         return false;
     }
 
-    private static Call<ResponseBody> buildRequest(Record record, IRestUpload uploadService) {
-        // TODO
-//        return uploadService.upload(
-//
-//        );
-        return null;
+    private static Call<ResponseBody> buildRequest(
+            Context context,
+            Record r,
+            IRestUpload uploadService
+    ) {
+        formdatatype = MediaType.parse("multipart/form-data");
+        LinkedList<MultipartBody.Part> parts = new LinkedList<>();
+        HashMap<String, RequestBody> fields = new HashMap<>();
+
+        // put images into multipart parts
+        for (int i = 0; i < r.images.length; i++) {
+            parts.add(getPartFromFileUri(context, "images[]", r.images[i]));
+        }
+
+        // put sound into multipart parts
+        parts.add(getPartFromFileUri(context, "sound", r.sound));
+
+        // and the rest of the mundane data into RequestBody objects
+        for (Field f : Record.class.getFields()) {
+            String field_name = f.getName();
+            String val;
+            switch (field_name) {
+                case "images":
+                case "sound":
+                    break; // Handled above => ignore here
+                case "roadkill":
+                    int roadkill = 0;
+                    try {
+                        roadkill = f.getBoolean(r)
+                                ? 1
+                                : 0;
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } finally {
+                        fields.put(field_name, getRequestBody(context, roadkill + ""));
+                    }
+                    break;
+                case "minelev":
+                case "maxelev":
+                case "year":
+                case "month":
+                case "day":
+                case "nestcount":
+                case "accuracy":
+                    val = null;
+                    try {
+                        val = f.getInt(r) + "";
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } finally {
+                        fields.put(field_name, getRequestBody(context, val));
+                    }
+                    break;
+                case "lat":
+                case "lng":
+                    val = null;
+                    try {
+                        val = String.format(java.util.Locale.US, "%.5f", f.getFloat(r));
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } finally {
+                        if (field_name.equals("lng")) field_name = "long";
+                        fields.put(field_name, getRequestBody(context, val));
+                    }
+                    break;
+                case "recordstatus":
+                    val = null;
+                    try {
+                        val = f.get(r).toString();
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } finally {
+                        fields.put(field_name, getRequestBody(context, val));
+                    }
+                    break;
+                default:
+                    val = null;
+                    try {
+                        val = (String) f.get(r);
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } finally {
+                        fields.put(field_name, getRequestBody(context, val));
+                    }
+                    break;
+            }
+        }
+
+        return uploadService.upload(
+                parts.get(0),
+                parts.get(1),
+                parts.get(2),
+                parts.get(3),
+                fields.get("username"),
+                fields.get("userid"),
+                fields.get("email"),
+                fields.get("project"),
+                fields.get("observers"),
+                fields.get("country"),
+                fields.get("province"),
+                fields.get("nearesttown"),
+                fields.get("locality"),
+                fields.get("minelev"),
+                fields.get("maxelev"),
+                fields.get("lat"),
+                fields.get("long"),
+                fields.get("accuracy"),
+                fields.get("source"),
+                fields.get("year"),
+                fields.get("month"),
+                fields.get("day"),
+                fields.get("note"),
+                fields.get("userdet"),
+                fields.get("nest_count"),
+                fields.get("nest_site"),
+                fields.get("roadkill"),
+                fields.get("taxonid"),
+                fields.get("taxonname"),
+                fields.get("institution_code"),
+                fields.get("collection"),
+                fields.get("recordbasis"),
+                fields.get("recordstatus")
+        );
+    }
+
+    private static MultipartBody.Part getPartFromFileUri(Context context, String key, Uri fileUri) {
+        if (fileUri == null) return null;
+        File file = FileUtils.getFile(context, fileUri);
+        RequestBody request = RequestBody.create(formdatatype, file);
+        MultipartBody.Part part = MultipartBody.Part.createFormData(key, file.getName(), request);
+        return part;
+    }
+
+    private static RequestBody getRequestBody(Context context, String value) {
+        if (value == null) return null;
+        RequestBody body = RequestBody.create(formdatatype, value);
+        return body;
     }
 }
+
